a41f41eb603dc4546abedc0d2fd58a12c2cdd33a
Sean Stephenson <seanmichaelstephenson@gmail.com>
2021-12-24 08:03:38 -0800

    more blocks

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const { Block, Blockchain } = require("./block.js");
 
 const MyChain = new Blockchain();
 
-const block = {
-    from: "John",
-    to: "Bob",
-    amount: 100,
-}
+const blocks = [
+    {
+        from: "Lincoln",
+        to: "Washington",
+        amount: 100,
+    },
+    {
+        from: "Jefferson",
+        to: "Adams",
+        amount: 70,
+    },
+    {
+        from: "Obama",
+        to: "Roosevelt",
+        amount: 105,
+    },
+]
 
-// Add a new block
-MyChain.addBlock(new Block(Date.now().toString(), block));
+blocks.forEach((block) => {
+  // Add a new block
+  MyChain.addBlock(new Block(Date.now().toString(), block));
+})
 
 // (This is just a fun example, real cryptocurrencies often have some more steps to implement).
 
